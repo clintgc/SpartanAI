753e48da0ab091eb81ed074892f318ac1d2e2f6d
Alen 'Dwayne Johnson' Peacock <alen@spacemonkey.com>
2025-12-17 22:01:15 -0700

    node / jest fixes

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,11 +1,14 @@
+const path = require('path');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/../'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  rootDir: path.resolve(__dirname),
+  roots: ['<rootDir>'],
+  testMatch: ['<rootDir>/unit/**/*.test.ts'],
   collectCoverageFrom: [
-    'functions/**/*.ts',
-    'shared/**/*.ts',
+    '../functions/**/*.ts',
+    '../shared/**/*.ts',
     '!**/*.d.ts',
     '!**/node_modules/**',
     '!**/dist/**',
@@ -14,5 +17,20 @@ module.exports = {
     '^spartan-ai-shared$': '<rootDir>/../shared',
   },
   setupFilesAfterEnv: ['<rootDir>/setup.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.json',
+    }],
+  },
+  // For workspace setups, include both local and parent node_modules
+  modulePaths: [
+    '<rootDir>/node_modules',
+    path.resolve(__dirname, '../node_modules'),
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  // Set NODE_PATH to help with module resolution in workspace
+  testEnvironmentOptions: {
+    NODE_PATH: path.resolve(__dirname, '../node_modules'),
+  },
 };
 
